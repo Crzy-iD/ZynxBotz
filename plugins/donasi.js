a195656eff56c33358b12c1b4c8cb9af1b4c8794
Crzy-iD <113808384+Crzy-iD@users.noreply.github.com>
2022-11-03 18:56:19 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -19,9 +19,9 @@ const catalog = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
 "firstImageId": 1,
 "salePriceAmount1000": "10000000",
 "retailerId": wm,
-"url": "wa.me/6281361281833"
+"url": "wa.me/62885814417473"
 },
-"businessOwnerJid": "6281361281833@s.whatsapp.net",
+"businessOwnerJid": "6285814417473@s.whatsapp.net",
 }
 }), { userJid: m.chat, quoted: ftroli })    
 
